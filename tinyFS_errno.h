722d63134198113d0f2b6fea69d17f779a620fe0
Nicholas Ambrose <nickambrose7@gmail.com>
2023-11-28 09:11:49 -0800

    added tinyFS files

---
+++ tinyFS_errno.h
@@ -0,0 +1,10 @@
+/*
+implement the codes as a set of statically defined macros.
+
+I'm going to just leave each error code as -1 with a print statement
+and then choose how to define our error codes later.
+
+Take a look at man 3 errno on the UNIX* machines for examples of the types of errors
+you might want to catch and report
+*/
+
